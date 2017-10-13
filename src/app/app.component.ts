237330b094fc43ce8b5e9773a68eb7dbad05d687
Yesenia <eliyessie@gmail.com>
2017-10-12 20:59:12 -0700

    pushing code from the class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,26 +6,40 @@ import {HttpClient} from "@angular/common/http";
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent implements OnInit {
+  public query: string;
+  public queryString: string;
   public postList;
   title = 'WTT';
-
-
 
   constructor(
     private http: HttpClient,
   ) {}
 
-  ngOnInit() {
-    this.http.get<Post>('https://www.reddit.com/r/php/search.json?q=cats&limit=5')
+  ngOnInit(){
+
+  }
+
+  searchPosts() {
+    console.log('bound!', this.query);
+    this.queryString = `https://www.reddit.com/r/all/search.json?q=${this.query}&limit=5`;
+    console.log('queryString', this.queryString);
+    this.getPosts();
+  }
+
+  getPosts(): void {
+    this.http.get<Post>(this.queryString)
     .subscribe(result => {
+      console.log('result', result);
       this.postList = result.data.children;
       console.log('this.postList', this.postList);
-      // this.postList.forEach(post => {})
+      this.postList.forEach(post => {
+        post.data.created = post.data.created * 1000;
+      })
     });
   }
 }
-
 
 
 export class Post {
