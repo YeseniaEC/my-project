5ec88bcf39c581d2f0d9e880072abae8bf7d438f
Yesenia <eliyessie@gmail.com>
2017-10-17 20:02:18 -0700

    fixed search in class, can now narrow down search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from "@angular/common/http";
+//PIPES
+import { SearchByPipe } from './pipes/search-by.pipe';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchByPipe
   ],
   imports: [
     BrowserModule,
